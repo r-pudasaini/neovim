b06b0f4a58dbd1d3cefe775fdf4d5ec6ee5559b3
Riyos_P <riyosp678@vt.edu>
2024-02-03 09:10:33 -0500

    Arrow keys no longer work in i mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,8 @@ vim.api.nvim_set_keymap("n", "<Up>", "<NOP>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Right>", "<NOP>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Left>", "<NOP>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Down>", "<NOP>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "<Up>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Right>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Left>", "<NOP>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Down>", "<NOP>", { noremap = true, silent = true })
